572b9748064c179aa7e1e770f4c493d4849498fc
luijar <luijar@luisatencio.net>
2018-02-19 11:23:44 -0500

    Preparing ESLint file

--- src/react-chat/src/App.test.js
+++ src/react-chat/src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
+// eslint-disable-next-line fp/no-nil
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
