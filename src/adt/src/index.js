4085da16263f4a3ed5d7d26f2e4988033f769c6e
luijar <luijar@luisatencio.net>
2018-03-18 00:37:54 -0400

    Renamed source back to src in adt project

--- src/adt/src/index.js
+++ src/adt/src/index.js
@@ -1,5 +1,5 @@
 // For compatibility reasons, needs to be using CommonJS convention
 // https://github.com/bookercodes/articles/blob/master/how-to-build-and-publish-es6-npm-modules-today-with-babel.md
 module.exports = {
-  Maybe: require('./src/maybe')
+  Maybe: require('./maybe')
 }
