bbd6effa34371e20178a31131c4ce56c2666e482
luijar <luijar@luisatencio.net>
2018-05-05 15:32:35 -0400

    remove color

--- src/fp-workshop/src/units/util.js
+++ src/fp-workshop/src/units/util.js
@@ -3,18 +3,8 @@ const { curry } = Combinators
 
 export const print = curry((msg, value) => {
   if (value['@@implements'] && value['@@implements'].includes('flatMap')) {
-    if (isNode()) {
-      console.log('%s\x1b[36m%s\x1b[0m', `${msg}:`.padEnd(50), value.toString())
-    } else {
-      console.log('%c Oh my heavens! ', 'background: #222; color: #bada55')
-    }
+    console.log('%s %s', `${msg}:`.padEnd(50), value.toString())
   } else {
-    if (isNode()) {
-      console.log(`%s\x1b[36m%s\x1b[0m`, `${msg}:`.padEnd(50), value)
-    }
+    console.log(`%s %s`, `${msg}:`.padEnd(50), value)
   }
 })
-
-const isNode = new Function(
-  'try {return this===global;}catch(e){return false;}'
-)
