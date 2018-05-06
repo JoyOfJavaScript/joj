ab65a2085e0d47ded7ed9b54cc269ad70fbdabf6
luijar <luijar@luisatencio.net>
2018-05-05 20:33:56 -0400

    Finished unit 3

--- src/fp-workshop/src/units/unit3.js
+++ src/fp-workshop/src/units/unit3.js
@@ -21,4 +21,29 @@ print('Raise two to the third.', raiseTwo(3))
 import { Combinators } from '../adt'
 const { curry } = Combinators
 
+//
+// Exercise 3 Build a simple curried logger function that accepts 3 parameters: format, level, and message
+//            The format should be either 'plain' or 'json' so that the log appears in said format
+//            Use this curried function to derive  'info' and 'error' logger functions from it.
+//
+const logger = curry((format, level, message) => {
+  if (format === 'json') {
+    console.log(
+      JSON.stringify({
+        date: new Date(),
+        level,
+        message,
+      })
+    )
+  } else {
+    console.log(`${new Date()} [%s] -> %s`, level, message)
+  }
+})
+
+const info = logger('plain', 'INFO')
+const error = logger('json', 'ERROR')
+
+info('Closing database connections')
+error('Error opening file!')
+
 console.log('--------------End of unit 3--------------')
