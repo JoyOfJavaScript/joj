a8763270738294619af47e0771eccd957c13a391
luijar <luijar@luisatencio.net>
2018-05-26 21:14:37 -0400

    Creating flow type definitions

---
+++ src/chapter-listings/src/ch01/countWords-flow.js
@@ -0,0 +1,32 @@
+// @flow
+import { expect } from 'chai'
+import fs from 'fs'
+import path from 'path'
+
+const decode = (charset: string = 'utf8') => (
+  buffer: buffer$Encoding
+): string => (!buffer ? '' : buffer.toString())
+
+const tokenize = (str: string): Array<string> => (str || '').split(/\s+/)
+
+const count = (arr: Array<string>) => (!arr ? 0 : arr.length)
+
+const compose = (...fns: Array<(any) => any>) => (x: any): any =>
+  fns.reduceRight((v, f) => f(v), x)
+
+const read = fs.readFileSync
+
+describe('Composition with types', () => {
+  it('Should count the words in a file using function composition and types', () => {
+    const countWords: string => number = compose(
+      count,
+      tokenize,
+      decode('utf8'),
+      read
+    )
+    const file = path.join(__dirname, '..//..', 'res', 'sample.txt')
+    const result = countWords(123)
+    console.log('Result is: ', result)
+    expect(result).to.be.equal(7)
+  })
+})
---
+++ src/chapter-listings/src/ch01/flow.js
@@ -0,0 +1,18 @@
+// // @flow
+// import { expect } from 'chai'
+// import fs from 'fs'
+// import path from 'path'
+
+// function foo(x: ?number): string {
+//   if (x) {
+//     return x
+//   }
+//   return 'default string'
+// }
+
+// function concat(a: string, b: string) {
+//   return a + b
+// }
+
+// concat('A', 'B') // Works!
+// concat(1, 2) // Error!
