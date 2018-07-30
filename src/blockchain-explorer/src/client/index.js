03aa1c7d1829bd73e078c9623bafb5159f55b209
luijar <luijar@luisatencio.net>
2018-07-30 10:07:41 -0400

    Promisify websocket on('connect')

--- src/blockchain-explorer/src/client/index.js
+++ src/blockchain-explorer/src/client/index.js
@@ -9,34 +9,41 @@
  */
 import { client as WebSocketClient } from 'websocket'
 import Menu from './Menu'
+import util from 'util'
 
 process.title = 'blockchain-explorer'
 
 const client = new WebSocketClient()
 
-client.on('connect', connection => {
-  console.log('Client Connected')
+const connect = util.promisify(client.on.bind(client, 'connect'))
 
-  connection.on('message', async message => {
-    const { type, utf8Data } = message
-    if (type === 'utf8') {
-      console.log("Received: '" + utf8Data + "'")
-      const action = await handleIncomingMessages(utf8Data)
-      sendAction(connection, action)
-    } else {
-      console.error(`Unable to handle message type ${type}`)
-    }
-  })
+connect().then(
+  err => {
+    console.log(`Major error ${err.message}`)
+  },
+  connection => {
+    console.log('Client Connected')
+    connection.on('message', async message => {
+      const { type, utf8Data } = message
+      if (type === 'utf8') {
+        console.log("Received: '" + utf8Data + "'")
+        const action = await handleIncomingMessages(utf8Data)
+        sendAction(connection, action)
+      } else {
+        console.error(`Unable to handle message type ${type}`)
+      }
+    })
 
-  connection.on('error', function (error) {
-    console.log('Connection Error: ' + error.toString())
-  })
-  connection.on('close', function () {
-    console.log('echo-protocol Connection Closed')
-  })
+    connection.on('error', function (error) {
+      console.log('Connection Error: ' + error.toString())
+    })
+    connection.on('close', function () {
+      console.log('echo-protocol Connection Closed')
+    })
 
-  connection.sendUTF(JSON.stringify({ action: '*' }))
-})
+    connection.sendUTF(JSON.stringify({ action: '*' }))
+  }
+)
 
 client.connect('ws://localhost:1337')
 
@@ -56,59 +63,9 @@ async function handleActionListing (actions) {
   })
 }
 
-function listActions (connection) {}
-
 function sendAction (connection, action) {
   console.log(`Sending action ${action}`)
   connection.sendUTF(JSON.stringify({ action }))
-}
-
-// client.onerror = function (event) {
-//   // an error occurred when sending/receiving data
-//   if (event.type === 'error') {
-//     console.log('Error recieved', event)
-//   }
-//   // After calling this function, no more events will be sent
-//   validateBlockchain$.unsubscribe()
-// }
-
-// client.onclose = () => {
-//   // After calling this function, no more events will be sent
-//   validateBlockchain$.unsubscribe()
-// }
-
-// client.onmessage = function (message) {
-//   // try to decode json (I assume that each message
-//   // from server is json)
-//   try {
-//     processResponse(message)
-//   } catch (e) {
-//     console.log(e)
-//   }
-//   // handle incoming message
-// }
-
-function processResponse (message) {
-  console.log(message)
-  const { status, action, payload } = JSON.parse(message.data)
-  if (status === 'Success') {
-    switch (action) {
-      case Actions.ADD_GENESIS:
-        state.push({ name: 'Genesis' })
-        break
-      case Actions.VALIDATE_BC:
-        if (payload.result) {
-          alert('Chain is valid!')
-        } else {
-          alert('Chain is NOT valid!')
-        }
-        break
-      default:
-        alert('Unknown action')
-    }
-  } else {
-    alert(`Error ocurred: ${payload.message}`)
-  }
 }
 
 /*
