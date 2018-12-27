9c52288db7fe30072b22a77925b6ca39405c5f94
luijar <luijar@luisatencio.net>
2018-12-27 17:24:00 -0500

    Refactored has signature to more orthogonal

--- src/blockchain/src/data/shared/HasSignature.js
+++ src/blockchain/src/data/shared/HasSignature.js
@@ -1,7 +1,8 @@
-import Maybe from '../../../../adt/dist/maybe'
 import SecureHandler from '../../common/SecureHandler'
 import sign from './has_signature/sign'
+import signInput from './has_signature/sign_input'
 import verify from './has_signature/verify'
+import verifySignatureInput from './has_signature/verify_signature_input'
 
 const DEFAULT_ENCODING_HEX = 'hex'
 const DEFAULT_SIGN_ALGO = 'RSA-SHA256'
@@ -35,38 +36,6 @@ export const HasSignature = (
     )
   }
 })
-
-/**
- * Signs the input data given a private key
- *
- * @param {Function} signer     Function used to sign
- * @param {string} privateKey   Private key used to sign
- * @param {string} input        Input data to sign
- * @return {string} Signed data
- * @throws {RangeError} In case any of actual arguments is invalid
- */
-const signInput = (signer, privateKey, input) =>
-  Maybe.of(k => i => k)
-    .ap(Maybe.fromNullable(privateKey))
-    .ap(Maybe.fromNullable(input))
-    .map(String)
-    .map(key => ({ key }))
-    .map(signer(input))
-    .getOrElseThrow(
-      new Error('Please provide valid arguments for [privateKey] and [input]')
-    )
-
-const verifySignatureInput = (verifier, publicKey, data, signature) =>
-  Maybe.of(k => s => d => [k, s, d])
-    .ap(Maybe.fromNullable(publicKey).map(String))
-    .ap(Maybe.fromNullable(signature))
-    .ap(Maybe.fromNullable(data))
-    .map(fields => verifier(...fields))
-    .getOrElseThrow(
-      new Error(
-        `Please provide valid arguments for publicKey: [${publicKey}], data: [${data}], and signature: [${signature}]`
-      )
-    )
 
 const signatureVerifier = !process.env.SECURE
   ? new Proxy(
