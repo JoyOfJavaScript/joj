31088f054ed36d4ce9b17b7573725c2049085f1a
Luis Atencio <luis.atencio@citrix.com>
2019-05-15 10:39:11 -0400

    Added more unit tests

--- src/blockchain/src/domain/shared/HasHash.spec.js
+++ src/blockchain/src/domain/shared/HasHash.spec.js
@@ -1,4 +1,5 @@
 import { assemble, computeCipher } from './has_hash'
+import HasHash from './HasHash'
 import { assert } from 'chai'
 
 describe('Has Hash Spec', () => {
@@ -19,9 +20,22 @@ describe('Has Hash Spec', () => {
       '04a635cf3f19a6dcc30ca7b63b9a1a6a1c42a9820002788781abae9bec666902'
     )
     const keys = ['sender', 'recipient', 'funds']
+    assert.equal(assemble(keys.map(k => obj[k])), '["luis@joj.com","luke@joj.com",10]')
+  })
+
+  it('Hashes a simple object literal', () => {
+    const hashTransaction = Object.assign(
+      {
+        sender: 'luis@joj.com',
+        recipient: 'luke@joj.com',
+        funds: 10
+      },
+      HasHash(['sender', 'recipient', 'funds'])
+    )
+
     assert.equal(
-      assemble(keys.map(k => obj[k])),
-      '["luis@joj.com","luke@joj.com",10]'
+      hashTransaction.calculateHash(),
+      '1d39e9fdd62716d90daa02756bef63f8c65fd38073bdd7722860254870668907'
     )
   })
 })
