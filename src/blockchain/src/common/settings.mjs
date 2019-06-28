cb7eb5af833aaf01f60592f156cc5c4353a3d8f0
Luis Atencio <luis.atencio@citrix.com>
2019-06-28 00:53:11 -0400

    Fix Docker run. Need to convert chapter listing modules to .mjs

--- src/blockchain/src/common/settings.mjs
+++ src/blockchain/src/common/settings.mjs
@@ -2,4 +2,4 @@ import Money from '../domain/value/Money.mjs'
 
 export const MINING_REWARD = Money('jsl', 12.5) // 12.5 jsl reward for miners
 export const MAX_NODES = 2 // Allow only 2 nodes in the network
-export const SYNC_TIMER = 10000 // 10-second intervals in the network
+export const SYNC_TIMER = 10_000 // 10-second intervals in the network
