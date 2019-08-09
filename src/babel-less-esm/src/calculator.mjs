998085a69b3b4b181c4e95abbe23f86a6a844ebd
Luis Atencio <luis.atencio@citrix.com>
2019-08-09 16:41:42 -0400

    Removing unnecessary project

--- src/babel-less-esm/src/calculator.mjs
+++
@@ -1,9 +0,0 @@
-export default class Calculator {
-  static multiply(a, b) {
-    return a * b
-  }
-
-  static add(a, b) {
-    return a + b
-  }
-}
--- src/babel-less-esm/src/index.js
+++
@@ -1,4 +0,0 @@
-import Calculator from './calculator.mjs'
-
-console.log(Calculator.multiply(2, 5))
-console.log(Calculator.add(2, 5))
