cda990b61a47d4f30002919069e3ac0d3870ebd1
Luis Atencio <luis.atencio@citrix.com>
2019-08-10 18:22:49 -0400

    Finish CH6

---
+++ src/chapter-listings/src/ch06/6-6-2.spec.mjs
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import { compose } from '@joj/blockchain/lib/fp/combinators.mjs'
+
+const { assert } = chai
+
+describe('6.6.2 - Orthogonal Architecture', () => {
+  it('takeSorted', () => {
+    function takeSorted(n, arr = []) {
+      const a = arr.sort()
+      return a.slice(0, n)
+    }
+    assert.deepEqual(takeSorted(2, [9, 5, 2]), [2, 5])
+  })
+  it('takeSorted (orthogonal)', () => {
+    const slice = n => arr => arr.slice(0, n)
+    const sort = arr => [...arr].sort()
+    const takeSorted = n =>
+      compose(
+        //#A
+        slice(n),
+        sort //#B
+      )
+    const original = [9, 5, 2]
+    assert.deepEqual(takeSorted(2)(original), [2, 5])
+    assert.deepEqual(original, [9, 5, 2])
+  })
+})
