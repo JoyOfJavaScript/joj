2a2b9d49171548d7bb6f54a026c9db27b7d5bd11
Luis Atencio <luis.atencio@citrix.com>
2019-12-02 08:22:42 -0500

    Comment out work in progress

--- src/blockchain/src/infrastructure/server/main.js
+++ src/blockchain/src/infrastructure/server/main.js
@@ -1,57 +1,57 @@
-const express = require("express")
-const bodyParser = require('body-parser')
-const WebSocket = require("ws")
+// const express = require("express")
+// const bodyParser = require('body-parser')
+// const WebSocket = require("ws")
 
-const http_port = process.env.HTTP_PORT || 3001
-const p2p_port = process.env.P2P_PORT || 6001
-const initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : []
-const peers = []
-let wss
+// const http_port = process.env.HTTP_PORT || 3001
+// const p2p_port = process.env.P2P_PORT || 6001
+// const initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : []
+// const peers = []
+// let wss
 
-const initHttpServer = () => {
+// const initHttpServer = () => {
 
-    const app = express()
-    app.use(bodyParser.json())
+//     const app = express()
+//     app.use(bodyParser.json())
 
-    app.get('/ping', (req, res) => {
-        const ping = "I'm alive"
-        console.log(ping)
-        broadcast(ping)
-        res.send(ping)
-    });
+//     app.get('/ping', (req, res) => {
+//         const ping = "I'm alive"
+//         console.log(ping)
+//         broadcast(ping)
+//         res.send(ping)
+//     });
 
-    app.listen(http_port, () => console.log('Listening http on port: ' + http_port))
-};
+//     app.listen(http_port, () => console.log('Listening http on port: ' + http_port))
+// };
 
 
-const initP2PServer = () => {
+// const initP2PServer = () => {
 
-    wss = new WebSocket.Server({ port: p2p_port })
-    wss.on('connection', (ws) => {
-        // do something and broadcast incoming message
-        ws.on('message', (data) => {
-            console.log(data)
-            broadcast(JSON.parse(data))
-        });
-    });
+//     wss = new WebSocket.Server({ port: p2p_port })
+//     wss.on('connection', (ws) => {
+//         // do something and broadcast incoming message
+//         ws.on('message', (data) => {
+//             console.log(data)
+//             broadcast(JSON.parse(data))
+//         });
+//     });
 
-    console.log('listening websocket p2p port on: ' + p2p_port)
+//     console.log('listening websocket p2p port on: ' + p2p_port)
 
-};
+// };
 
-const initPeers = (initialPeers) => {
-    initialPeers.forEach((peer) => {
-        const ws = new WebSocket(peer)
-        peers.push(ws)
-    })
-};
+// const initPeers = (initialPeers) => {
+//     initialPeers.forEach((peer) => {
+//         const ws = new WebSocket(peer)
+//         peers.push(ws)
+//     })
+// };
 
-var broadcast = (data) => {
-    peers.forEach(ws => {
-        ws.send(JSON.stringify(data))
-    });
-}
+// var broadcast = (data) => {
+//     peers.forEach(ws => {
+//         ws.send(JSON.stringify(data))
+//     });
+// }
 
-initHttpServer();
-initP2PServer();
-initPeers(initialPeers)
+// initHttpServer();
+// initP2PServer();
+// initPeers(initialPeers)
