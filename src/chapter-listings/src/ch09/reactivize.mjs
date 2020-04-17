6f36256f4202ca59d146bd6e9212eaadadd0dfed
Luis Atencio <luis.atencio@citrix.com>
2020-04-16 23:42:28 -0400

    Checked in raw scratch pad files for CH8 and 9

---
+++ src/chapter-listings/src/ch09/reactivize.mjs
@@ -0,0 +1,63 @@
+
+import EventEmitter from 'events'
+
+function implementsPushProtocol(obj) {
+    return obj
+        && Symbol.iterator in Object(obj)
+        && typeof obj['push'] === 'function'
+        && typeof obj[Symbol.iterator] === 'function'
+}
+
+const ON_EVENT = 'on'
+const END_EVENT = 'end'
+const LOG_LABEL = `IN-STREAM`
+const LOG_LABEL_INNER = `${LOG_LABEL}:push`
+
+export const reactivize = obj => {
+
+    implementsPushProtocol(obj) || throw new TypeError('Object does not implement a push protocol')
+
+    const emitter = new EventEmitter()
+
+    const pushProxy = new Proxy(obj, {
+        get(...args) {
+            const [target, key] = args
+            if (key === 'push') {
+                const pushRef = target[key]
+                return (...capturedArgs) => {
+                    const result = pushRef.call(target, ...[capturedArgs])
+                    emitter.emit(ON_EVENT, ...capturedArgs)
+                    return result
+                }
+            }
+            return Reflect.get(...args)
+
+        }
+    })
+    const observable = {
+        [Symbol.observable]() {
+            return new Observable(observer => {
+                console.group(LOG_LABEL)
+                emitter.on(ON_EVENT, newValue => {
+                    console.group(LOG_LABEL_INNER)
+                    console.log('Emitting new value: ', newValue)
+                    observer.next(newValue)
+                    console.groupEnd(LOG_LABEL_INNER)
+                })
+                emitter.on(END_EVENT, () => {
+                    observer.complete()
+                })
+                for (const value of obj) {
+                    observer.next(value)
+                }
+                return () => {
+                    console.groupEnd(LOG_LABEL)
+                    emitter.removeAllListeners(ON_EVENT, END_EVENT)
+                }
+            })
+        }
+    }
+    return Object.assign(pushProxy, observable)
+}
+
+export default reactivize
---
+++ src/chapter-listings/src/ch09/rx.mjs
@@ -0,0 +1,228 @@
+import 'core-js/features/observable/index.js'
+import { Readable } from 'stream'
+import { curry } from '@joj/blockchain/util/fp/combinators.js'
+
+
+export const listen$ = curry((eventName, element) => {
+    return new Observable(observer => {
+        // Create an event handler which sends data to the sink
+        const handler = event => observer.next(event)
+
+        // Attach the event handler
+        element.addEventListener(eventName, handler, true)
+
+        // Return a cleanup function which will cancel the event stream
+        return _ => element.removeEventListener(eventName, handler, true)
+    })
+})
+
+if (!Observable.fromGenerator || typeof Observable.fromGenerator !== 'function') {
+    // Must be:
+    // - writable: false
+    // - enumerable: false
+    // - configurable: false
+    Object.defineProperty(Observable, 'fromGenerator', {
+        value(generator) {
+            return new Observable(observer => {
+                Readable
+                    .from(generator)
+                    .on('data', :: observer.next)
+                    .on('end',  :: observer.complete)
+            })
+        },
+        enumerable: false,
+        writable: false,
+        configurable: false
+    })
+}
+
+export const throttle = curry((limit, stream) => {
+    let lastRan = 0
+    let lastInterval = 0
+    return new Observable(observer => {
+        const subs = stream.subscribe({
+            next(value) {
+                if (!lastRan) {
+                    observer.next(value)
+                    lastRan = Date.now()
+                } else {
+                    clearTimeout(lastInterval)
+                    lastInterval = setTimeout(() => {
+                        if (Date.now() - lastRan >= limit) {
+                            observer.next(value)
+                            lastRan = Date.now()
+                        }
+                    }, limit - (Date.now() - lastRan))
+                }
+            },
+            error(e) {
+                observer.error(e)
+            },
+            complete() {
+                observer.complete()
+            }
+        })
+        return () => subs.unsubscribe()
+    })
+})
+
+export const map = curry(
+    (fn, stream) =>
+        new Observable(observer => {
+            const subs = stream.subscribe({
+                next(value) {
+                    try {
+                        observer.next(fn(value))
+                    }
+                    catch (err) {
+                        observer.error(err)
+                    }
+                },
+                error(e) {
+                    observer.error(e)
+                },
+                complete() {
+                    observer.complete()
+                }
+            })
+            return () => subs.unsubscribe()
+        })
+)
+
+export const filter = curry(
+    (predicate, stream) =>
+        new Observable(observer => {
+            const subs = stream.subscribe({
+                next(value) {
+                    if (predicate(value)) {
+                        observer.next(value)
+                    }
+                },
+                error(e) {
+                    observer.error(e)
+                },
+                complete() {
+                    observer.complete()
+                }
+            })
+            return () => subs.unsubscribe()
+        })
+)
+
+
+export const buffer = curry(
+    (count, stream) => {
+        const _internalStorage = []
+        return new Observable(observer => {
+            const subs = stream.subscribe({
+                next(value) {
+                    _internalStorage.push(value)
+                    if (_internalStorage.length >= count) {
+                        // emit as array                        
+                        observer.next(_internalStorage)
+                        _internalStorage.length = 0 // clear
+                    }
+                },
+                error(e) {
+                    observer.error(e)
+                },
+                complete() {
+                    observer.complete()
+                }
+            })
+            return () => subs.unsubscribe()
+        })
+    }
+)
+
+export const skip = curry(
+    (count, stream) => {
+        let skipped = 0
+        return new Observable(observer => {
+            const subs = stream.subscribe({
+                next(value) {
+                    if (skipped++ >= count) {
+                        observer.next(value)
+                    }
+                },
+                error(e) {
+                    observer.error(e)
+                },
+                complete() {
+                    observer.complete()
+                }
+            })
+            return () => subs.unsubscribe()
+        })
+    }
+)
+
+export const reduce = curry(
+    (accumulator, initialValue, stream) => {
+        let result = initialValue ?? {}
+        return new Observable(observer => {
+            const subs = stream.subscribe({
+                next(value) {
+                    result = accumulator(result, value)
+                },
+                error(e) {
+                    observer.error(e)
+                },
+                complete() {
+                    observer.next(result)
+                    observer.complete()
+                }
+            })
+            return () => subs.unsubscribe()
+        })
+    }
+)
+
+export const mapTo = curry(
+    (value, stream) => {
+        return new Observable(observer => {
+            const subs = stream.subscribe({
+                next() {
+                    observer.next(value)
+                },
+                error(e) {
+                    observer.error(e)
+                },
+                complete() {
+                    observer.complete()
+                }
+            })
+            return () => subs.unsubscribe()
+        })
+    }
+)
+
+
+export const ReactiveExtensions = {
+    filter(predicate) {
+        return filter(predicate, this)
+    },
+    map(fn) {
+        return map(fn, this)
+    },
+    buffer(count) {
+        return buffer(count, this)
+    },
+    skip(count) {
+        return skip(count, this)
+    },
+    reduce(accumulator, initialValue = {}) {
+        return reduce(accumulator, initialValue, this)
+    },
+    mapTo(value) {
+        return mapTo(value, this)
+    },
+    throttle(limit) {
+        return throttle(limit, this)
+    }
+}
+
+
+// Extend native Observable object
+Object.assign(Observable.prototype, ReactiveExtensions);
+
